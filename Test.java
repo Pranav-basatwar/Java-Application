5e8e28aca654c351910eedb276bc1e90ca148b7e
Pranav-basatwar <pranav.basatwar@gmail.com>
2025-10-27 13:52:10 +0530

    Add Test.java with a Hello World message

---
+++ Test.java
@@ -0,0 +1,11 @@
+class Test{
+
+public static void main(String args[]){
+
+    System.out.println("---------------------------------------");
+      System.out.println("Hello world from Github");
+    System.out.println("---------------------------------------");
+
+
+}
+}
